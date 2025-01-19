0885ba8a24a85a0c287ec48f393515539543cbf2
smofktn <meijifj0215@gmail.com>
2025-01-19 20:06:55 +0900

    [update]アクセストークンの期限を出力するように変更(#17)

--- src/repository/getAccessToken.ts
+++ src/repository/getAccessToken.ts
@@ -5,7 +5,10 @@ export const getAccessToken = async (): Promise<void> => {
   try {
     const data = await spotifyApi.clientCredentialsGrant();
     const accessToken = data.body["access_token"];
+    const expiresIn = data.body["expires_in"];
+
     console.log("Access Token:", accessToken);
+    console.log("Token Expires In", expiresIn);
 
     // APIクライアントにアクセストークンをセット
     return spotifyApi.setAccessToken(accessToken);
